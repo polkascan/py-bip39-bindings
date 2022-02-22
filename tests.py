3f3115f2eec4b8d95b6a5570d170d2c3e38e72a7
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2022-02-22 12:39:10 +0100

    Added multi-language support (#7)

--- tests.py
+++ tests.py
@@ -30,11 +30,23 @@ class MyTestCase(unittest.TestCase):
         mnemonic = bip39.bip39_generate(12)
         self.assertTrue(bip39.bip39_validate(mnemonic))
 
+    def test_generate_mnemonic_french(self):
+        mnemonic = bip39.bip39_generate(12, 'fr')
+        self.assertTrue(bip39.bip39_validate(mnemonic, 'fr'))
+
     def test_generate_invalid_mnemonic(self):
         self.assertRaises(ValueError, bip39.bip39_generate, 13)
 
     def test_validate_mnemonic(self):
         self.assertTrue(bip39.bip39_validate(self.mnemonic))
+
+    def test_validate_mnemonic_zh_hans(self):
+        self.assertTrue(bip39.bip39_validate('观 敲 荣 硬 责 雪 专 宴 醇 飞 图 菌', 'zh-hans'))
+
+    def test_validate_mnemonic_fr(self):
+        self.assertTrue(bip39.bip39_validate(
+            'moufle veinard tronc magasin merle amour toboggan admettre biotype décembre régalien billard', 'fr'
+        ))
 
     def test_invalidate_mnemonic(self):
         self.assertFalse(bip39.bip39_validate("invalid mnemonic"))
@@ -42,14 +54,45 @@ class MyTestCase(unittest.TestCase):
     def test_mini_seed(self):
         self.assertEqual(self.mini_secret, bip39.bip39_to_mini_secret(self.mnemonic, ''))
 
+    def test_mini_seed_zh_hans(self):
+
+        mini_secret = bip39.bip39_to_mini_secret('观 敲 荣 硬 责 雪 专 宴 醇 飞 图 菌', '', 'zh-hans')
+        self.assertEqual(
+            [60, 215, 169, 79, 32, 218, 203, 59, 53, 155, 18, 234, 160, 215, 97, 30, 176, 243, 224, 103, 240, 114, 170,
+             26, 4, 63, 250, 164, 88, 148, 41, 68], mini_secret)
+
     def test_invalid_mini_seed(self):
         self.assertRaises(ValueError, bip39.bip39_to_mini_secret, 'invalid mnemonic', '')
 
     def test_seed(self):
         self.assertEqual(self.seed, bip39.bip39_to_seed(self.mnemonic, ''))
 
+    def test_seed_zh_hans(self):
+        mnemonic = '旅 滨 昂 园 扎 点 郎 能 指 死 爬 根'
+        seed = bip39.bip39_to_seed(mnemonic, '', 'zh-hans')
+
+        self.assertEqual(
+            '3e349679fd7fb457810d578a8b63237c6ba1fd09b39d7f33650c0f879a2cdc46',
+            bytes(seed).hex()
+        )
+
+    def test_seed_fr(self):
+        mnemonic = 'moufle veinard tronc magasin merle amour toboggan admettre biotype décembre régalien billard'
+        seed = bip39.bip39_to_seed(mnemonic, '', 'fr')
+
+        self.assertEqual(
+            'fe7ca72e2de46c24f121cf649057202ffdd9a51e63fc9fd98f8614fc68c6bbff',
+            bytes(seed).hex()
+        )
+
     def test_invalid_seed(self):
         self.assertRaises(ValueError, bip39.bip39_to_seed, 'invalid mnemonic', '')
+
+    def test_invalid_language_code(self):
+        with self.assertRaises(ValueError) as e:
+            bip39.bip39_generate(12, "unknown")
+
+        self.assertEqual('Invalid language_code', str(e.exception))
 
 
 if __name__ == '__main__':
